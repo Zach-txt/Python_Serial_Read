ccedd72f68c991f0c50bd5d6895fc612a7c6ff8d
Zachary Hollands Laptop <zachary.hollands@usherbrooke.ca>
2024-02-14 15:46:59 -0500

    First attempt to read and write. (No test done)

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 #Includes
-import serial
+import sys
 
-ser = serial.Serial(port='PUT THE COM PORT', baudrate=9600)
+import serial
+from datetime import date
+
+ser = serial.Serial(port='COM2', baudrate=9600)
+today = date.today()
 
 while(1):
     value= ser.readline()
     valueInString =str(value, 'UTF-8')
     print(valueInString)
+
+    if(valueInString != ""):#Vérifier ce qu'on recoit vraiment
+        f = open("C:\Ecole\PMC\S7\Serial Read\Raw data/" + today, "a")
+        f.write(""+valueInString)
+        f.close()
 
 #def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
